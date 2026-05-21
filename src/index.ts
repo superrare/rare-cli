b5978a5209b259a3a37b8c025d3b3c1a8c67341a
Keegan <keegan@superrare.com>
2026-05-21 14:36:42 -0400

    Stop loading cwd dotenv in CLI (#62)

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,5 @@
-import { loadDotEnv } from './env.js';
 import { printError } from './errors.js';
 import { createRareProgram } from './program.js';
-
-loadDotEnv();
 
 const program = createRareProgram();
 
